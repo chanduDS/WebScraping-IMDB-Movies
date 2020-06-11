b38e2a16a2b959eb492202306076476fd79e8b0d
Chandra <sreechandraduppala1@gmail.com>
2020-06-11 22:00:12 +0530

    First Commit

---
+++ Library/ConfigReader.py
@@ -0,0 +1,12 @@
+from configparser import ConfigParser
+# reading config file
+def readConfigData(section, key):
+    config = ConfigParser()
+    config.read("../ConfigurationFiles/Config.ini")
+    return config.get(section, key)
+
+def read_urls(genre,val):
+    url="https://www.imdb.com/search/title/?genres="+genre+"&start="+str(val)
+    return url
+# print(readConfigData('Details','Application_URL'))
+# running the method and checking if it returns what we want
